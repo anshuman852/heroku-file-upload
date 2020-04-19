70d3e691754b63aeb88b61983461829d29e74b1d
anshuman <anshuman@firehound.org>
2020-04-19 13:10:23 +0530

    Change logic

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
+const Randomstring=require("randomstring")
 const multer = require("multer");
 const path = require('path');
+var fs = require('fs');
 const router = express.Router();
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -9,19 +11,21 @@ var storage = multer.diskStorage({
   },
   filename: function (req, file, cb) {
     name = file.originalname.split(".");
+    rend=Randomstring.generate(7)
+    fs.mkdirSync("./files/"+rend)
     cb(
       null,
-      name[0] + "-" + Math.floor(Math.random() * 1000 + 1) + "." + name[1]
+      rend+ "/"+file.originalname
     );
   },
 });
 
 var uploads = multer({ storage: storage });
-app.use("/files", express.static("files"));
+app.use(express.static("files"));
 app.get("/", (req, res) => res.sendFile(path.join(__dirname+'/index.html')));
 app.post("/upload", uploads.single("file"), function (req, res) {
-  baseurl = "https://rekt-files.herokuapp.com";
-  res.send(baseurl + "/files/" + res.req.file.filename);
+  baseurl = "https://rekt-files.herokuapp.com/";
+  res.send(baseurl + res.req.file.filename);
 });
 
 port = process.env.PORT || 8080;

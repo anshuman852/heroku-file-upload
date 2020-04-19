464040401395319f53068fb34ca6e25596ffea24
anshuman <anshuman@firehound.org>
2020-04-19 12:38:50 +0530

    Add a homepage

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const multer = require("multer");
+const path = require('path');
+const router = express.Router();
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "files/");
@@ -16,13 +18,14 @@ var storage = multer.diskStorage({
 
 var uploads = multer({ storage: storage });
 app.use("/files", express.static("files"));
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => res.sendFile(path.join(__dirname+'/index.html')));
 app.post("/upload", uploads.single("file"), function (req, res) {
   baseurl = "https://rekt-files.herokuapp.com";
   res.send(baseurl + "/files/" + res.req.file.filename);
 });
 
 port = process.env.PORT || 8080;
+app.use("/",router)
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
 );

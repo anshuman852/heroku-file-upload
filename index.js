3fa8e5a739ad3fba60403a3aa871906bdd438817
anshuman <anshuman852@gmail.com>
2020-07-30 04:43:46 +0530

    Add postgres

--- index.js
+++ index.js
@@ -1,61 +1,94 @@
-const express = require('express')
-const app = express()
-const Randomstring = require('randomstring')
-const multer = require('multer')
-const path = require('path')
-const bodyParser = require("body-parser")
-var fs = require('fs')
-const request=require('request')
-
-const router = express.Router()
-
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+const express = require("express");
+const app = express();
+const Randomstring = require("randomstring");
+const multer = require("multer");
+const path = require("path");
+const bodyParser = require("body-parser");
+var fs = require("fs");
+const { Sequelize, DataTypes } = require("sequelize");
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  protocol: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // <<<<<<< YOU NEED THIS
+    },
+  } /* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */,
+});
+const router = express.Router();
+const Files = sequelize.define("Files", {
+  fileid: {
+    type: DataTypes.STRING,
+    primaryKey: true,
+    allowNull: false,
+  },
+  filelink: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
 app.use(bodyParser.json());
 
-
+let rend;
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'files/')
+    cb(null, "files/");
   },
-  filename: function (req, file, cb) {
-    name = file.originalname.split('.')
-    rend = Randomstring.generate(7)
-    fs.mkdirSync('./files/' + rend)
-    cb(null, rend + '/' + file.originalname)
-  }
-})
-var uploads = multer({ storage: storage })
-app.use(express.static('files'))
-
-// routes 
-app.get("/", (req, res) => res.sendFile(path.join(__dirname + '/index.html')));
-
-app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')))
-
-app.post('/upload', uploads.single('file'), function (req, res) {
-  baseurl = 'https://rekt-files.herokuapp.com/'
-  res.send(encodeURI(baseurl + res.req.file.filename));
-})
-
+  filename: async function (req, file, cb) {
+    name = file.originalname.split(".");
+    rend = Randomstring.generate(7);
+    fs.mkdirSync("./files/" + rend);
+    const feel = await Files.create({
+      fileid: rend,
+      filelink:
+        "https://rekt-files.herokuapp.com/" +
+        rend +
+        "/" +
+        file.originalname,
+    });
+    cb(null, rend + "/" + file.originalname);
+  },
+});
 var uploads = multer({ storage: storage });
 app.use(express.static("files"));
-app.get("/", (req, res) => res.sendFile(path.join(__dirname+'/index.html')));
-app.get("/file",(req,res) =>{
-  url=req.query.url
-  rend = Randomstring.generate(7)
-  ext=get_url_extension(url)
-  res.header('Content-Disposition', `attachment; filename="${rend}.${ext}"`);
-  request.get(url).pipe(res)
+
+app.get("/", (req, res) => res.sendFile(path.join(__dirname + "/index.html")));
+
+app.post("/upload", uploads.single("file"), function (req, res) {
+  baseurl = "https://rekt-files.herokuapp.com/";
+  res.send(encodeURI(baseurl + "dl/" + rend));
 });
-app.get("/upload", (req, res) => res.sendFile(path.join(__dirname+'/index.html')));
+app.get("/dl/:id", async (req, res) => {
+  id = req.params.id;
+  file = await Files.findByPk(id)
+    .then((data) => {
+      res.redirect(data.filelink);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(404).send("Not found");
+    });
+});
+var uploads = multer({ storage: storage });
+app.use(express.static("files"));
+app.get("/", (req, res) => res.sendFile(path.join(__dirname + "/index.html")));
+
+app.get("/upload", (req, res) =>
+  res.sendFile(path.join(__dirname + "/index.html"))
+);
 port = process.env.PORT || 8080;
-app.use("/",router)
+Files.sync({ force: true });
+app.use("/", router);
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
-)
-function get_url_extension( url ) {
-  return url.split(/[#?]/)[0].split('.').pop().trim();
+);
+function get_url_extension(url) {
+  return url.split(/[#?]/)[0].split(".").pop().trim();
 }
